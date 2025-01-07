40b0cae96b5b4fd15e31e99108a333199503d29c
Hyuk <hyukho83@gmail.com>
2025-01-08 05:42:58 +0900

    Update Navbar component with Audio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <About />
