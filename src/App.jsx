1c0338591b4391e59313f76d0286d6e9bd1b3a40
Hyuk <hyukho83@gmail.com>
2025-01-08 04:45:25 +0900

    chage background color

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
       <Navbar />
       <Hero />
       <About />
