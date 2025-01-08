40e34b5ecf0e57a9a239f3a2b98f4a7b1ee730ed
Hyuk <hyukho83@gmail.com>
2025-01-09 08:26:38 +0900

    Add an image & mouse move event handler

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,6 +1,47 @@
 import AnimatedTitle from "./AnimatedTitle";
+import { useRef } from "react";
+import gsap from "gsap";
 
 export default function Story() {
+  const frameRef = useRef(null);
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
   return (
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -13,6 +54,22 @@ export default function Story() {
             sectionId="#story"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contai"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
